2b2f0237170bbb7504cb43d57191fafab4a8e403
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-08-06 01:07:53 +0100

    fix: ssh config for darwin 1password

--- system/shared/home-manager.nix
+++ system/shared/home-manager.nix
@@ -39,7 +39,11 @@ in
 
   programs.ssh = {
     enable = true;
-    matchBlocks."*".extraOptions.IdentityAgent = "~/.1password/agent.sock";
+    matchBlocks."*".extraOptions.IdentityAgent = (
+      if isDarwin
+        then "\"~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
+        else "~/.1password/agent.sock"
+    );
   };
 
   programs.git = {
