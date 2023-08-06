49875d30c7e558cad0c0f64d84114772b68c25ed
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-08-06 01:59:32 +0100

    fix: staff on mac, not wheel

--- system/darwin/configuration.nix
+++ system/darwin/configuration.nix
@@ -16,7 +16,7 @@
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-      trusted-users = [ "@wheel" ];
+      trusted-users = [ "@staff" ];
       warn-dirty = false;
     };
   };
