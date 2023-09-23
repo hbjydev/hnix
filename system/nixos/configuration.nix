7df5c430561b370fef1e2c7ef8307d59821835d9
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-09-23 22:47:55 +0100

    fix(lint): format file

--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -3,14 +3,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports = 
+  imports =
     [ ./modules/boot.nix ./modules/nix.nix ] # Standard ('required') modules
     ++ lib.lists.forEach options (opt:
       if opt == "x"
-        then (import ./modules/x.nix { inherit username; })
-        else if opt == "docker"
-          then (import ./modules/docker.nix { inherit username; })
-        else ./modules/${opt}.nix
+      then (import ./modules/x.nix { inherit username; })
+      else if opt == "docker"
+      then (import ./modules/docker.nix { inherit username; })
+      else ./modules/${opt}.nix
     ); # Opt-in modules
 
   programs._1password.enable = true;
