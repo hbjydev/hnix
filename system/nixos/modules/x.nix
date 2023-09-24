a848499ff358a3373c3ca74036ce115faf038e8b
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-09-24 19:13:13 +0100

    feat(x): exclude packages

--- system/nixos/modules/x.nix
+++ system/nixos/modules/x.nix
@@ -13,8 +13,36 @@
       enable = true;
       autoSuspend = false;
     };
+
+    excludePackages = with pkgs; [
+      xterm
+    ];
+
     desktopManager.gnome.enable = true;
   };
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gnome-console
+  ] ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    gnome-maps
+    gnome-calendar
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    yelp
+  ]));
 
   # Fonts config
   fonts = {
