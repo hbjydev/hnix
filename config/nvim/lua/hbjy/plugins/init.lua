fb42cf8ddbf60d046acb58e5be38cff772a5d711
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-09-28 16:05:53 +0100

    feat(neovim): copilot

--- config/nvim/lua/hbjy/plugins/init.lua
+++ config/nvim/lua/hbjy/plugins/init.lua
@@ -3,6 +3,7 @@ local function init()
   require('hbjy.plugins.neotree').init()
   require('hbjy.plugins.lsp').init()
   require('hbjy.plugins.cmp').init()
+  require('hbjy.plugins.copilot').init()
   require('hbjy.plugins.indent_blankline').init()
   require('hbjy.plugins.neorg').init()
 
---
+++ system/shared/modules/neovim.nix
@@ -0,0 +1,128 @@
+{ inputs }:
+
+{ pkgs, ... }:
+
+let
+  vim-just = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-just";
+    src = pkgs.fetchFromGitHub {
+      owner = "NoahTheDuke";
+      repo = "vim-just";
+      rev = "927b41825b9cd07a40fc15b4c68635c4b36fa923";
+      sha256 = "sha256-BmxYWUVBzTowH68eWNrQKV1fNN9d1hRuCnXqbEagRoY=";
+    };
+  };
+in
+
+{
+  programs.neovim = {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+
+    plugins = with pkgs; [
+      # lsp
+      vimPlugins.nvim-lspconfig
+      vimPlugins.nvim-treesitter.withAllGrammars
+      vimPlugins.rust-tools-nvim
+      vimPlugins.fidget-nvim
+
+      # completion
+      vimPlugins.nvim-cmp
+      vimPlugins.cmp-path
+      vimPlugins.cmp-buffer
+      vimPlugins.cmp-nvim-lsp
+      vimPlugins.cmp-nvim-lsp-signature-help
+      vimPlugins.copilot-lua
+
+      # telescope
+      vimPlugins.plenary-nvim
+      vimPlugins.popup-nvim
+      vimPlugins.telescope-nvim
+      vimPlugins.nvim-treesitter-context
+      vimPlugins.telescope-ui-select-nvim
+      vimPlugins.todo-comments-nvim
+
+      # theme
+      vimPlugins.oxocarbon-nvim
+      vimPlugins.indent-blankline-nvim
+      vimPlugins.comment-nvim
+      vimPlugins.gitsigns-nvim
+
+      # other
+      vimPlugins.nui-nvim
+      vimPlugins.gitsigns-nvim
+      vimPlugins.neo-tree-nvim
+      vimPlugins.nvim-web-devicons
+      vimPlugins.editorconfig-nvim
+      vimPlugins.neorg
+      vimPlugins.neorg-telescope
+      vimPlugins.zen-mode-nvim
+      vim-just
+
+      # my config
+      inputs.self.packages.${pkgs.system}.hbjy-nvim
+    ];
+
+    extraConfig = ''
+      lua << EOF
+        require 'hbjy'.init()
+      EOF
+    '';
+
+    extraPackages = with pkgs; [
+      # languages
+      jsonnet
+      nodejs
+      python310Full
+      rustc
+
+      # LSPs
+      gopls
+      jsonnet-language-server
+      lua-language-server
+      nil
+      nodePackages."bash-language-server"
+      nodePackages."diagnostic-languageserver"
+      nodePackages."dockerfile-language-server-nodejs"
+      #nodePackages."pyright"
+      nodePackages."typescript"
+      nodePackages."typescript-language-server"
+      nodePackages."vscode-langservers-extracted"
+      nodePackages."yaml-language-server"
+
+      python311Packages."python-lsp-server"
+
+      python311Packages."pyls-flake8"
+      python311Packages."flake8"
+
+      python311Packages."python-lsp-black"
+      python311Packages."black"
+
+      python311Packages."pyls-isort"
+      python311Packages."isort"
+
+      python311Packages."pylsp-mypy"
+      python311Packages."mypy"
+
+      python311Packages."pylsp-rope"
+
+      rust-analyzer
+      terraform-ls
+
+      # formatters
+      gofumpt
+      golines
+      nixpkgs-fmt
+      python310Packages.black
+      rustfmt
+
+      # tools
+      cargo
+      gcc
+      lazydocker
+      yarn
+    ];
+  };
+}
