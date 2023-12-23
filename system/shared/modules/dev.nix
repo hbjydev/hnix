63b724a3646c71b3d3452f8adc463fac97f3cfa3
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-12-23 16:35:13 +0000

    feat(k9s): new k9s theme

--- system/shared/modules/dev.nix
+++ system/shared/modules/dev.nix
@@ -1,4 +1,7 @@
 { ... }:
+let
+  oxocarbon = (import ../oxocarbon.nix).dark;
+in
 {
   programs.go = {
     enable = true;
@@ -12,6 +15,101 @@
         logoless = true;
       };
     };
-    skin = (builtins.readFile ../../../config/k9s/skin.yml);
+
+    skin = {
+      k9s = {
+        body = {
+          fgColor   = oxocarbon.base04;
+          bgColor   = oxocarbon.base00;
+          logoColor = oxocarbon.base0A;
+        };
+
+        prompt = {
+          fgColor      = oxocarbon.base04;
+          bgColor      = oxocarbon.base00;
+          suggestColor = oxocarbon.base03;
+
+          border = {
+            default = oxocarbon.base0A;
+            command = oxocarbon.base0A;
+          };
+        };
+
+        info = {
+          fgColor        = oxocarbon.base04;
+          selectionColor = oxocarbon.base04;
+        };
+
+        frame = {
+          border = {
+            fgColor    = oxocarbon.base02;
+            focusColor = oxocarbon.base0C;
+          };
+
+          menu = {
+            fgColor     = oxocarbon.base04;
+            keyColor    = oxocarbon.base0A;
+            numKeyColor = oxocarbon.base0A;
+          };
+
+          crumbs = {
+            fgColor     = oxocarbon.base04;
+            bgColor     = oxocarbon.base00;
+            activeColor = oxocarbon.base01;
+          };
+
+          status = {
+            newColor       = oxocarbon.base0E;
+            modifyColor    = oxocarbon.base0B;
+            addColor       = oxocarbon.base0B;
+            errorColor     = oxocarbon.base0A;
+            highlightcolor = oxocarbon.base0A;
+            killColor      = oxocarbon.base0A;
+            completedColor = oxocarbon.base03;
+          };
+
+          title = {
+            fgColor        = oxocarbon.base04;
+            bgColor        = oxocarbon.base00;
+            highlightColor = oxocarbon.base0C;
+            counterColor   = oxocarbon.base04;
+            filterColor    = oxocarbon.base04;
+          };
+        };
+
+        views = {
+          table = {
+            fgColor       = oxocarbon.base04;
+            bgColor       = oxocarbon.base00;
+            cursorBgColor = oxocarbon.base01;
+            cursorFgColor = oxocarbon.base00;
+            markColor     = oxocarbon.base0C;
+
+            header = {
+              fgColor = oxocarbon.base03;
+              bgColor = oxocarbon.base00;
+            };
+          };
+
+          yaml = {
+            keyColor = oxocarbon.base0A;
+            colonColor = oxocarbon.base03;
+            valueColor = oxocarbon.base04;
+          };
+
+          logs = {
+            fgColor = oxocarbon.base04;
+            bgColor = oxocarbon.base00;
+
+            indicator = {
+              fgColor = oxocarbon.base03;
+              bgColor = oxocarbon.base00;
+            };
+          };
+        };
+      };
+    };
   };
+
+  #home.file.".config/k9s/skin.yml".source = ../../../config/k9s/skin.yml;
 }
