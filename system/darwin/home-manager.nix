82bd33f8e6156a8c06e5ef9c225d48c5c17baabf
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-01-16 13:45:37 +0000

    feat(kitty): new tab bar config

--- system/darwin/home-manager.nix
+++ system/darwin/home-manager.nix
@@ -36,15 +36,31 @@ in
     };
 
     settings = {
+      allow_remote_control = "yes";
+      enabled_layouts = "splits";
+      hide_window_decorations = "titlebar-and-corners";
+      listen_on = "unix:/tmp/kitty";
+      macos_option_as_alt = "left";
+      macos_quit_when_last_window_closed = "yes";
+      macos_titlebar_color = "system";
+      tab_bar_edge = "top";
+      tab_bar_style = "powerline";
+      wayland_titlebar_color = "system";
+
       active_border_color = "#ee5396";
       active_tab_background = "#ee5396";
       active_tab_foreground = "#161616";
-      allow_remote_control = "yes";
       background = "#161616";
       background_opacity = "0.9";
       bell_border_color = "#ee5396";
       color0 = "#262626";
       color1 = "#ff7eb6";
+      color10 = "#42be65";
+      color11 = "#82cfff";
+      color12 = "#33b1ff";
+      color13 = "#ee5396";
+      color14 = "#3ddbd9";
+      color15 = "#ffffff";
       color2 = "#42be65";
       color3 = "#82cfff";
       color4 = "#33b1ff";
@@ -53,30 +69,17 @@ in
       color7 = "#dde1e6";
       color8 = "#393939";
       color9 = "#ff7eb6";
-      color10 = "#42be65";
-      color11 = "#82cfff";
-      color12 = "#33b1ff";
-      color13 = "#ee5396";
-      color14 = "#3ddbd9";
-      color15 = "#ffffff";
       cursor = "#f2f4f8";
       cursor_text_color = "#393939";
-      enabled_layouts = "splits";
       foreground = "#dde1e6";
-      hide_window_decorations = "titlebar-and-corners";
       inactive_border_color = "#ff7eb6";
       inactive_tab_background = "#393939";
       inactive_tab_foreground = "#dde1e6";
-      listen_on = "unix:/tmp/kitty";
-      macos_option_as_alt = "left";
-      macos_quit_when_last_window_closed = "yes";
-      macos_titlebar_color = "system";
       selection_background = "#525252";
       selection_foreground = "#f2f4f8";
       tab_bar_background = "#161616";
       url_color = "#ee5396";
       url_style = "single";
-      wayland_titlebar_color = "system";
     };
   };
 
