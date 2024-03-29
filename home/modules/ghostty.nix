02d63ded607eca0d78fb7856546e53070d5059b6
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-03-29 22:27:52 +0000

    feat(ghostty): init with oxocarbon theming

--- home/modules/ghostty.nix
+++ home/modules/ghostty.nix
@@ -1,6 +1,54 @@
 { inputs, pkgs, ... }:
+let
+  oxocarbonTheme = import ../oxocarbon.nix;
+  oxocarbon = oxocarbonTheme.dark;
+in
 {
+  fonts.fontconfig.enable = true;
+
   home.packages = [
     inputs.ghostty.packages.${pkgs.system}.default
+    (pkgs.nerdfonts.override {
+      fonts = [ "IntelOneMono" ];
+    })
   ];
+
+  home.file.".config/ghostty.config".text = ''
+    background-opacity = 0.9
+    font-family = IntoneMono Nerd Font
+    font-size = 14
+    macos-option-as-alt = true
+
+    # black
+    palette = 0=#${oxocarbon.base00}
+    palette = 8=#${oxocarbon.base03}
+
+    # red
+    palette = 1=#${oxocarbon.base08}
+    palette = 9=#${oxocarbon.base09}
+
+    # green
+    palette = 2=#${oxocarbon.base0B}
+    palette = 10=#${oxocarbon.base01}
+
+    # yellow
+    palette = 3=#${oxocarbon.base0A}
+    palette = 11=#${oxocarbon.base02}
+
+    # blue
+    palette = 4=#${oxocarbon.base0D}
+    palette = 12=#${oxocarbon.base04}
+
+    # purple
+    palette = 5=#${oxocarbon.base0E}
+    palette = 13=#${oxocarbon.base06}
+
+    # aqua
+    palette = 6=#${oxocarbon.base0C}
+    palette = 14=#${oxocarbon.base0F}
+
+    # white
+    palette = 7=#${oxocarbon.base05}
+    palette = 15=#${oxocarbon.base07}
+  '';
 }
