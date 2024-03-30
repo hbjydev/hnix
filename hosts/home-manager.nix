f3949d74322d88424c4782b4ece94e4e2cac7786
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-03-30 01:09:59 +0000

    feat(phoebe): remove kitty for now

--- hosts/home-manager.nix
+++ hosts/home-manager.nix
@@ -9,7 +9,7 @@ in
 {
   imports = if desktop then [
     shared-config
-    (import ../home/modules/kitty.nix)
+    # (import ../home/modules/kitty.nix)
     (import ../home/modules/ghostty.nix)
   ] else [ shared-config ];
 
