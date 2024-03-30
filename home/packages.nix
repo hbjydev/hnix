1e0198642ee8204878e1dbe5d2446006c9f97c8f
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-03-30 10:43:47 +0000

    chore(cleanup): stop importing all evals from nixpkgs for home-manager

--- home/packages.nix
+++ home/packages.nix
@@ -1,34 +1,35 @@
 { pkgs, inputs, work, ... }:
 
-with pkgs; [
+[
   (pkgs.callPackage ../pkgs/bins {})
 
-  _1password
-  awscli2
-  bun
-  cachix
-  ctlptl
-  doppler
-  fd
-  glab
-  glow
-  gum
-  httpie
-  jq
-  k9s
-  kind
-  kubectl
-  mods
-  moreutils
-  nmap
-  podman-compose
-  pyenv
-  ripgrep
-  sops
-  tilt
-  viddy
+  pkgs._1password
+  pkgs.awscli2
+  pkgs.bun
+  pkgs.cachix
+  pkgs.ctlptl
+  pkgs.doppler
+  pkgs.fd
+  pkgs.glab
+  pkgs.glow
+  pkgs.gum
+  pkgs.httpie
+  pkgs.jq
+  pkgs.k9s
+  pkgs.kind
+  pkgs.kubectl
+  pkgs.mods
+  pkgs.moreutils
+  pkgs.nmap
+  pkgs.podman-compose
+  pkgs.pyenv
+  pkgs.ripgrep
+  pkgs.sops
+  pkgs.tilt
+  pkgs.viddy
+  pkgs.weechat
 
   inputs.hvim.packages.${pkgs.system}.default # my nixos config
 ] ++ pkgs.lib.lists.optionals work [
-  bitwarden-cli
+  pkgs.bitwarden-cli
 ]
