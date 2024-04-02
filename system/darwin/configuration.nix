86143d724dfd9af3bc181a6e90e16a2d326fb627
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-04-02 11:19:56 +0100

    feat(nix): add ghostty cachix to trusted subs

--- system/darwin/configuration.nix
+++ system/darwin/configuration.nix
@@ -12,11 +12,13 @@
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [
         "https://nix-community.cachix.org"
+        "https://ghostty.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       ];
-      trusted-users = [ "@staff" ];
+      trusted-users = [ "@staff" username ];
       warn-dirty = false;
     };
   };
