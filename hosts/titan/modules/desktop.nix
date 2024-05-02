14d67db32965b1d36080a31035815a7b2a8c0d67
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-05-02 20:58:32 +0100

    fix(titan): clean up arrays

--- hosts/titan/modules/desktop.nix
+++ hosts/titan/modules/desktop.nix
@@ -32,28 +32,32 @@
     xclip
   ];
 
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
-    gnome-tour
-    gnome-console
-    gedit # text editor
-  ] ++ (with pkgs.gnome; [
-    cheese # webcam tool
-    gnome-music
-    gnome-terminal
-    gnome-maps
-    gnome-calendar
-    #epiphany # web browser
-    geary # email reader
-    evince # document viewer
-    gnome-characters
-    totem # video player
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
-    atomix # puzzle game
-    yelp
-  ]));
+  environment.gnome.excludePackages = (
+    (with pkgs; [
+      gnome-photos
+      gnome-tour
+      gnome-console
+      gedit # text editor
+    ])
+    ++
+    (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      gnome-terminal
+      gnome-maps
+      gnome-calendar
+      #epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+      yelp
+    ])
+  );
 
   # Fonts config
   fonts = {
@@ -66,7 +70,9 @@
     };
 
     packages = [
-      pkgs.nerdfonts
+      (pkgs.nerdfonts.override {
+        fonts = [ "GeistMono" ];
+      })
     ];
   };
 
