14b30ebc973e0cf8d227eb02141000055574a5ba
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-06-20 09:31:58 +0100

    feat: nixpkgs alloy module

--- nixos/roles/alloy/default.nix
+++ nixos/roles/alloy/default.nix
@@ -1,27 +1,17 @@
-{ pkgs-nix, config, pkgs, ... }:
+{ config, ... }:
 {
-  imports = [ pkgs-nix.nixosModules.alloy ];
-
-  services.alloy = {
-    enable = true;
-    package = pkgs-nix.packages.${pkgs.system}.alloy;
-    openFirewall = true;
-    environmentFiles = [ config.sops.secrets.gc_token.path ];
-    extraArgs = "--stability.level public-preview";
-    configPath = "/etc/alloy";
-    group = "root";
-    user = "root";
-  };
-
   sops.secrets.gc_token = {
     owner = config.services.alloy.user;
     restartUnits = [ "alloy.service" ];
     sopsFile = ../../../secrets/grafana-cloud.yaml;
   };
 
-  environment.etc."alloy/config.alloy" = {
-    source = ./config.alloy;
-    mode = "0440";
-    user = config.services.alloy.user;
+  environment.etc."alloy/config.alloy".source = ./config.alloy;
+
+  services.alloy = {
+    enable = true;
+    extraArgs = ["--stability.level=public-preview"];
   };
+
+  systemd.services.alloy.serviceConfig.EnvironmentFile = [ config.sops.secrets.gc_token.path ];
 }
