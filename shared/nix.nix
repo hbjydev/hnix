5a2a856d96d4f2ab7286518347a2cd674c9472de
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-06-20 13:06:54 +0100

    fix: include staff group for macos

--- shared/nix.nix
+++ shared/nix.nix
@@ -15,7 +15,7 @@
       allowed-users = [ "@wheel" ];
       build-users-group = "nixbld";
       builders-use-substitutes = true;
-      trusted-users = [ "root" "@wheel" ];
+      trusted-users = [ "root" "@wheel" "@staff" ];
       sandbox = hostType == "nixos";
       substituters = [
         "https://nix-community.cachix.org"
