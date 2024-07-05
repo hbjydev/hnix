6d6b41ed9b0dce280d7e2fca463b268cbf5ffb17
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2024-07-05 08:22:51 +0100

    fix: include my cache in the flake nixConfig

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://cache.hayden.moe/hayden"
       "https://nix-community.cachix.org"
       "https://ghostty.cachix.org"
       "https://altf4llc-os.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "hayden:11XFX2/dPhIIdk2tXAcX63EgFDSNyHfISMozZ9iw7M8="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       "altf4llc-os.cachix.org-1:9mjdTiW005IKsaVtapDSvkbskvoQ7t4/Q5S3FsQJ/5A="
